d6d68829781fb7f75c2eef2db3cf5ad7088609aa
peaky76 <robertjamespeacock@gmail.com>
2024-03-10 15:14:42 +0000

    feat: build_guessed_word and join_guessed_letters

--- hangman_cli/hangman.py
+++ hangman_cli/hangman.py
@@ -3,11 +3,25 @@ from pathlib import Path
 from random import choice
 
 
+def build_guessed_word(target_word, guessed_letters):
+    current_letters = []
+    for letter in target_word:
+        if letter in guessed_letters:
+            current_letters.append(letter)
+        else:
+            current_letters.append("_")
+    return " ".join(current_letters)
+
+
 def get_player_input(guessed_letters):
     while True:
         player_input = input("Guess a letter: ").lower()
         if _validate_input(player_input, guessed_letters):
             return player_input
+
+
+def join_guessed_letters(guessed_letters):
+    return " ".join(sorted(guessed_letters))
 
 
 def select_word():
