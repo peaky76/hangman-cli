6a6d65821bd08370d349d630e3f68b2520d27238
peaky76 <robertjamespeacock@gmail.com>
2024-11-08 23:50:29 +0000

    feat: game loop

--- hangman_cli/hangman.py
+++ hangman_cli/hangman.py
@@ -2,6 +2,8 @@ import string
 from pathlib import Path
 from random import choice
 
+from hangman_cli.hanged_man import HANGED_MAN
+
 MAX_INCORRECT_GUESSES = 6
 
 def build_guessed_word(target_word, guessed_letters):
@@ -33,7 +35,7 @@ def join_guessed_letters(guessed_letters):
 
 def select_word():
     cwd = Path(__file__).parent
-
+    
     with Path(f"{cwd}/words.txt").open() as words:
         word_list = words.readlines()
         
@@ -46,3 +48,38 @@ def _validate_input(player_input, guessed_letters):
         and player_input in string.ascii_lowercase
         and player_input not in guessed_letters
     )
+
+if __name__ == "__main__":
+    # Initial setup
+    target_word = select_word()
+    guessed_letters = set()
+    guessed_word = build_guessed_word(target_word, guessed_letters)
+    wrong_guesses = 0
+    print("Welcome to Hangman!")
+
+    # Game loop
+    while not game_over(wrong_guesses, target_word, guessed_letters):
+        print(HANGED_MAN[wrong_guesses])
+        print(f"Your word is: {guessed_word}")
+        print(
+            "Current guessed letters: "
+            f"{join_guessed_letters(guessed_letters)}\n"
+        )
+
+        player_guess = get_player_input(guessed_letters)
+        if player_guess in target_word:
+            print("Great guess!")
+        else:
+            print("Sorry, it's not there.")
+            wrong_guesses += 1
+
+        guessed_letters.add(player_guess)
+        guessed_word = build_guessed_word(target_word, guessed_letters)
+
+    # Game over
+    print(HANGED_MAN[wrong_guesses])
+    if wrong_guesses == MAX_INCORRECT_GUESSES:
+        print("Sorry, you lost!")
+    else:
+        print("Congrats! You did it!")
+    print(f"Your word was: {target_word}")
