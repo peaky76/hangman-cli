1a916b099ad34290d35248a2eb221071b6314fb3
peaky76 <robertjamespeacock@gmail.com>
2024-11-08 23:28:29 +0000

    fix: remove unintended comment

--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -14,7 +14,7 @@ def test_build_guessed_word():
     target_word = "hangman"
     guessed_letters = ["a", "n", "g"]
     expected_result = "_ a n g _ a n"
-    assert build_guessed_word(target_word, guessed_letters) == expected_result  # noqa: F821def test_get_player_input():
+    assert build_guessed_word(target_word, guessed_letters) == expected_result  # noqa: F821
 
 
 def test_get_player_input_with_valid_input(mocker):
